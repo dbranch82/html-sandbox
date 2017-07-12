ddb4143040766005e95e89e81a682bbb6757318d
Danielle Branch <dbranch82@gmail.com>
2017-07-12 14:00:29 -0600

    added base html learned front end

--- index.php
+++ index.php
@@ -6,5 +6,40 @@
 	</head>
 	<body>
 		<h1>This is my web page</h1>
+
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<!-- This is an HTML comment -->
+		<h2>A few of my favorite things</h2>
+		<ul>
+			<li>Tacos</li>
+			<li>The beach</li>
+			<li>Kittens</li>
+			<li>Fast cars</li>
+		</ul>
+
+		<h2>Things to do today</h2>
+		<ol>
+		<li>Feed the kitty</li>
+			<li>Figure out how to code</li>
+
+		</ol>
 	</body>
 </html>
