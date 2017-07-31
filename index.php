1ef28ce70972ba679d8c579845e9e72a13ff10d4
dbranch6 <dbranch6@cnm.edu>
2017-07-31 08:47:17 -0600

    test

--- index.php
+++ index.php
@@ -1,47 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>HTML Sandbox</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"
+		<title>Bootstrap</title>
 	</head>
 	<body>
-		<h1>This is my web page</h1>
-
-		<table>
-			<tr>
-				<th>Cats</th>
-				<th>Dogs</th>
-			</tr>
-			<tr>
-				<td>Persian</td>
-				<td>Beagle</td>
-			</tr>
-			<tr>
-				<td>Maine Coon</td>
-				<td>Rottweiler</td>
-			</tr>
-			<tr>
-				<td>Siamese</td>
-				<td>German Shepard</td>
-			</tr>
-		</table>
-
-		<!-- This is an HTML comment -->
-		<h2>A few of my favorite things</h2>
-		<ul>
-			<li>Tacos</li>
-			<li>The beach</li>
-			<li>Kittens</li>
-			<li>Fast cars</li>
-		</ul>
-
-		<h2>Things to do today</h2>
-		<ol>
-		<li>Feed the kitty</li>
-			<li>Figure out how to code</li>
-
-		</ol>
-		<h2>I Love Pikachu</h2>
-		<img src="images/ufc.jpg" alt="Pikachu"/>
+Hello!
 	</body>
 </html>
